5402246c0f259e0e23c7418419c3d167f70082e2
Khaled Saad <elgendy1204@gmail.com>
2019-02-07 20:11:41 +0200

    Reverse integer exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,7 +7,18 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+const reverseString = require('../reversestring');
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let isNegative = false;
+    if(n < 0) {
+        isNegative = true;
+    }
+    const stringNumber = n.toString();
+    if(isNegative) {
+        return parseInt(reverseString(stringNumber)) * -1;
+    }
+    return parseInt(reverseString(stringNumber));
+}
 
 module.exports = reverseInt;
