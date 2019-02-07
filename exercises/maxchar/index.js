69785d3b855c96d2ca3f5262d7d1e1a8751b3c7f
Khaled Saad <elgendy1204@gmail.com>
2019-02-07 20:48:00 +0200

    Finish max character function

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charactersMap = {};
+    for (var i = 0; i < str.length; i++) {
+        if(charactersMap[str[i]]) {
+            charactersMap[str[i]] = charactersMap[str[i]] + 1;
+        } else {
+            charactersMap[str[i]] = 1;
+        }
+    }
+    let maxNumber = 0;
+    let mostCharUsed;
+    for (const char in charactersMap) {
+        if(maxNumber <= parseInt(charactersMap[char])) {
+            mostCharUsed = char;
+            maxNumber = parseInt(charactersMap[char]);
+        }
+    }
+    return mostCharUsed;
+}
 
 module.exports = maxChar;
