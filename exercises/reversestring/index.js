8b2e8e22ab5c7cbc3722424343577d8f58e869e8
Khaled Saad <elgendy1204@gmail.com>
2019-02-07 19:57:48 +0200

    Finish first example

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversedString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString = reversedString + str[i];
+    }
+    return reversedString;
+}
 
 module.exports = reverse;
