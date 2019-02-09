a17da2e6d8e38e90ef208ade46567db921a0fa7e
Khaled Saad <elgendy1204@gmail.com>
2019-02-09 19:49:16 +0200

    Finish Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const stringAObject = {};
+    const stringBObject = {};
+    let isAnagrams = true;
+    for (var i = 0; i < stringA.length; i++) {
+        const character = stringA[i].toLowerCase();
+        if(character.match(/[a-z]/i)) {
+            stringAObject[character] = stringAObject[character] ? stringAObject[character] + 1 : 1;
+        }
+    }
+    for (var i = 0; i < stringB.length; i++) {
+        const character = stringB[i].toLowerCase();
+        if(character.match(/[a-z]/i)) {
+            stringBObject[character] = stringBObject[character] ? stringBObject[character] + 1 : 1;
+        }
+    }
+    if(Object.keys(stringAObject).length !== Object.keys(stringBObject).length) {
+        isAnagrams = false;
+        return isAnagrams;
+    }
+    for(let key in stringAObject) {
+        if(stringAObject[key] !== stringBObject[key]) {
+            isAnagrams = false;
+            return isAnagrams;
+        }
+    }
+    return isAnagrams;
+}
 
 module.exports = anagrams;
