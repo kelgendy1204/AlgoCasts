3f6ac5ea904e1894e6532fbb70c81b60d271bed6
Khaled Saad <elgendy1204@gmail.com>
2019-02-09 18:31:35 +0200

    Finish chunks

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        if(!newArray[newArray.length - 1] || newArray[newArray.length - 1].length >= size) {
+            newArray.push([]);
+            newArray[newArray.length - 1].push(array[i]);
+        } else {
+            newArray[newArray.length - 1].push(array[i]);
+        }
+    }
+    console.log(newArray);
+    return newArray;
+}
 
 module.exports = chunk;
