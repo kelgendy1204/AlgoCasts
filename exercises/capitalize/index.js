55cfcbead5cff288ecdc5bd0155ce8c49da562c0
Khaled Saad <elgendy1204@gmail.com>
2019-02-09 19:55:06 +0200

    Finish capitalizing

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,9 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    const strArray = str.split(' ').map(word => `${word[0].toUpperCase()}${word.substr(1)}`).join(' ');
+    return strArray;
+}
 
 module.exports = capitalize;
