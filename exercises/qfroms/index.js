656df6f9a0ce91a566959f80b40e111b3b601a89
Khaled Saad <elgendy1204@gmail.com>
2019-02-10 21:50:36 +0200

    Make queue from 2 stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,38 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }
+
+    add(item) {
+        this.stackA.push(item);
+    }
+
+    remove() {
+        while(this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        const lastItem = this.stackB.pop();
+        while(this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+        return lastItem;
+    }
+
+    peek() {
+        while(this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        const lastItem = this.stackB.peek();
+        while(this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+        return lastItem;
+    }
+
+}
 
 module.exports = Queue;
